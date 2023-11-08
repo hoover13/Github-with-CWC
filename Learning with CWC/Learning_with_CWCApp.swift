2355954d17f4f23e23aa5fce74e403fc89caf5cf
Min Thu Khine <helloworldjustin9@gmail.com>
2023-11-07 22:34:39 -0500

    Initial Commit

---
+++ Learning with CWC/Learning_with_CWCApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_with_CWCApp.swift
+//  Learning with CWC
+//
+//  Created by Hoover on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Learning_with_CWCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
